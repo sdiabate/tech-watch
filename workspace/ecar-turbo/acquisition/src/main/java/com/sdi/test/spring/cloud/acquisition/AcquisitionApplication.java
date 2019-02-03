aad73acfefc560df2a59f2b6411aff67df52c76d
sdiabate <diabate_seriba@yahoo.fr>
2019-02-03 22:40:00 +0100

    Initial commit

---
+++ workspace/ecar-turbo/acquisition/src/main/java/com/sdi/test/spring/cloud/acquisition/AcquisitionApplication.java
@@ -0,0 +1,14 @@
+package com.sdi.test.spring.cloud.acquisition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquisitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcquisitionApplication.class, args);
+	}
+
+} 
+
